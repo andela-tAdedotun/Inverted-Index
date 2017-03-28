81a6b49380e03d6caaae8703256f2ebb3459f3e6
Taiwo <andeladeveloper@Andelas-MacBook-Pro.local>
2017-03-28 11:22:42 +0100

    Congigured files to allow coverage reporting to Coveralls.io

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,12 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage', 'coveralls'],
+
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage/'
+    }
 
 
     // web server port
