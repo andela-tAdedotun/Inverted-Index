efce69b51a532c85f9abcf025dbedaf3d0ec1838
Taiwo <andeladeveloper@Andelas-MacBook-Pro.local>
2017-04-06 18:08:19 +0100

    move localStorage related logic to app.js.

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,15 @@
 (() => {
-  angular.module('invertedIndex', ['angular-typed']).controller('InvertedController', ($scope) => {
+  angular.module('invertedIndex', ['angular-typed'])
+  .controller('InvertedController', ($scope) => {
     const scope = $scope;
 
     scope.newIndex = new InvertedIndex();
     scope.uploadedFiles = {};
     scope.indexedFiles = {};
     scope.tableHeads = [];
-    scope.recentlyIndexed = scope.newIndex.getRecentlyIndexed();
-    scope.moreThanOneStored = scope.recentlyIndexed.length > 1;
     scope.searchString = document.getElementById('search').innerHTML;
 
-    scope.indexInLocalStorage = scope.newIndex.indexInLocalStorage();
+    // scope.indexInLocalStorage = scope.newIndex.indexInLocalStorage();
 
     scope.displayCreate = false;
     scope.displayIndex = false;
@@ -20,11 +19,11 @@
       return objectIsEmpty;
     };
 
-    function displayMessage(message) {
+    const displayMessage = (message) => {
       scope.message = message;
       $('#response-modal').modal();
       scope.$evalAsync();
-    }
+    };
 
     scope.readFile = (inputDom) => {
       const files = [];
@@ -58,13 +57,21 @@
       const fileChoice = scope.fileToIndex;
 
       try {
-        const createdIndex = scope.newIndex
-                              .createIndex(fileChoice, scope.uploadedFiles[fileChoice]);
-        scope.indexedFiles[fileChoice] = createdIndex[0];
-        scope.justIndexed = createdIndex[1];
+        const createdIndex =
+        scope.newIndex.createIndex(fileChoice, scope.uploadedFiles[fileChoice]);
+        // console.log(createdIndex);
+        scope.indexedFiles[fileChoice] = createdIndex[fileChoice][0];
+
+        const indexedDocs = angular.fromJson(localStorage.indexedDocs);
+        indexedDocs[fileChoice] =
+           [scope.indexedFiles[fileChoice], createdIndex[fileChoice][2]];
+        localStorage.indexedDocs = angular.toJson(indexedDocs);
+
+        scope.justIndexed = createdIndex[fileChoice][1];
         const indexedFilesList = Object.keys(scope.indexedFiles);
 
-        scope.tableHeads = scope.newIndex.getTitles(scope.uploadedFiles[fileChoice]);
+        scope.tableHeads =
+         scope.newIndex.getTitles(scope.uploadedFiles[fileChoice]);
         scope.fileToSearch = indexedFilesList[indexedFilesList.length - 1];
 
         if (indexedFilesList.length > 1) {
@@ -78,28 +85,59 @@
       }
     };
 
+    scope.recentlyIndexed = () => {
+      if (!localStorage.indexedDocs) {
+        localStorage.indexedDocs = JSON.stringify({});
+      }
+
+      const allRecentlyIndexed =
+         Object.keys(JSON.parse(localStorage.indexedDocs));
+      return allRecentlyIndexed;
+    };
+
+    scope.indexInLocalStorage = () => {
+      const filesInStorage =
+          Object.keys(angular.fromJson(localStorage.indexedDocs));
+      if (filesInStorage.length === 0) {
+        return false;
+      }
+
+      return true;
+    };
+
+    scope.moreThanOneStored = scope.recentlyIndexed().length > 1;
+
     scope.searchIndex = () => {
+      scope.fileIndices = angular.fromJson(localStorage.indexedDocs);
+      // scope.tableHeads = fileIndices[2];
       scope.searchResult = {};
 
       if (scope.fileToSearch === 'All Files') {
-        scope.searchResult = scope.newIndex.searchIndex('All Files', scope.searchString, scope.indexedFiles);
+        scope.searchResult = scope.newIndex.searchIndex('All Files'
+        , scope.searchString, scope.fileIndices);
         scope.displayIndex = false;
         scope.allFiles = true;
       } else {
         scope.searchResult[scope.fileToSearch] =
-                scope.newIndex.searchIndex(scope.fileToSearch, scope.searchString);
+            scope.newIndex.searchIndex(scope.fileToSearch
+                 , scope.searchString, scope.fileIndices);
       }
 
       scope.displayIndex = false;
     };
 
     scope.getIndex = (fileName) => {
-      scope.indexedFiles[fileName] = scope.newIndex.getIndex(fileName)[0];
+      const fileIndices = angular.fromJson(localStorage.indexedDocs)[fileName];
+      // console.log(fileIndices)
+
+      // return fileIndices;
+
+      scope.indexedFiles[fileName] = fileIndices[0];
       scope.uploadedFiles[fileName] = [scope.indexedFiles[fileName]];
       scope.justIndexed = fileName;
 
       const indexedFilesList = Object.keys(scope.indexedFiles);
-      scope.tableHeads = scope.newIndex.getIndex(fileName)[1];
+      scope.tableHeads = fileIndices[1];
       scope.fileToSearch = indexedFilesList[indexedFilesList.length - 1];
 
       if (indexedFilesList.length > 1) {
@@ -108,14 +146,20 @@
 
       scope.displayCreate = false;
       scope.displayIndex = true;
+
+      return fileIndices;
     };
 
     scope.deleteIndex = (fileName) => {
-      scope.newIndex.deleteIndex(fileName);
       if (fileName === 'Delete All') {
-        scope.recentlyIndexed = [];
+        localStorage.indexedDocs = angular.toJson({});
+        delete scope.recentlyIndexed();
       } else {
-        scope.recentlyIndexed.splice(scope.recentlyIndexed.indexOf(fileName), 1);
+        const fileIndices = angular.fromJson(localStorage.indexedDocs);
+        delete fileIndices[fileName];
+        localStorage.indexedDocs = angular.toJson(fileIndices);
+        scope.recentlyIndexed()
+           .splice(scope.recentlyIndexed().indexOf(fileName), 1);
       }
     };
   });
